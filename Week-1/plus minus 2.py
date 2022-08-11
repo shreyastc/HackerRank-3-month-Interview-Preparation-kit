9cf352ffb67733e3aca74650fb5bb26dfe1d46a7
Shreyas T C <46958001+shreyastc@users.noreply.github.com>
2022-08-11 23:17:58 +0530

    Plus minus code comments added

--- Week-1/plus minus 2.py
+++ Week-1/plus minus 2.py
@@ -14,18 +14,20 @@ def plusMinus(arr):
     positive_count = 0
     negative_count = 0
     zero_count = 0
+    #Initialising the count of positive, negative and zero numbers as 0
     
     for num in arr:
         if num > 0:
-            positive_count += 1
+            positive_count += 1   #If number is greater than 0, then increase the positive count
         elif num < 0:
-            negative_count += 1
+            negative_count += 1   #If number is kesser than 0, then increase the negative count
         else:
-            zero_count += 1
+            zero_count += 1    #Else increase the zero count
+            
         
-    print(positive_count / len(arr)) 
-    print(negative_count / len(arr))
-    print(zero_count / len(arr))
+    print(positive_count / len(arr)) #Print the ratio of postive number count by dividing it by length of total size of array
+    print(negative_count / len(arr))   #Print the ratio of negative number count by dividing it by length of total size of array
+    print(zero_count / len(arr))   #Print the ratio of zero count by dividing it by length of total size of array
     
 if __name__ == '__main__':
     n = int(input().strip())
